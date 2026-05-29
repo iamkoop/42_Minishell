97da0e3a77133b017b510a2f73f4191840872038
Nils Druon <nildruon@c1r5p4.42vienna.com>
2026-05-29 15:57:27 +0200

    echo now works correctly

--- src/builtin_functions/echo.c
+++ src/builtin_functions/echo.c
@@ -6,47 +6,52 @@
 /*   By: nildruon <nildruon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/27 14:41:32 by nildruon          #+#    #+#             */
-/*   Updated: 2026/05/27 19:11:05 by nildruon         ###   ########.fr       */
+/*   Updated: 2026/05/29 15:56:44 by nildruon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-static void print_input(char	**input, int start)
-{
-	while (input[start])
-	{
-		ft_putstr_fd(input[start],1);
-		if(input[start + 1])
-			ft_putchar_fd(' ', 1);
-		start++;
-	}
-	
-}
-
-//we are asumming that the input is echo word1/flag word2 word3 etc..
-//TODO: Handle -nnnn and check with peers what they did with the other flags..
-void echo(char	**input)
+static int is_validFlag(char	*str, int	*print_nl)
 {
 	int len;
+	int i;
 
-	len = 0;
-	while (input[len])
-		len++;
-	if(len == 1)
+	len = ft_strlen(str);
+	if(len <= 1)
+		return(0);
+	i = 0;
+	if(str[i++] != '-')
+		return(0);
+	while (str[i])
 	{
-		ft_putchar_fd('\n',1);
-		return ;
+		if(str[i] != 'n')
+			return(0);
+		i++;
 	}
-	if(ft_strlen(input[1]) == 2 && ft_strncmp(input[1], "-", 1) == 0)
+	*print_nl = 0;
+	return(1);
+}
+ 
+void echo(char	**input)
+{
+	int print_nl;
+	int print;
+	int i;
+
+	print = 0;
+	print_nl = 1;
+	i = 1;
+	while (input[i] && is_validFlag(input[i], &print_nl))
+			i++;
+	while (input[i])
 	{
-		if(len == 2)
-			return ;
-		print_input(input, 2);
+		ft_putstr_fd(input[i],1);
+		if(input[i +1])
+			ft_putchar_fd(' ', 1);
+		i++;
 	}
-	else
-	{
-		print_input(input, 1);
+	
+	if(print_nl)
 		ft_putchar_fd('\n', 1);
-	}
 }
