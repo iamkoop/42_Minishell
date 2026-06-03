2cfafddca032e14a87cb9cf4b10ca75a54e1bb4f
Nils Druon <ndruon@outlook.com>
2026-06-03 14:55:44 +0200

    added the skeleton of the cd func

--- src/builtin_functions/cd.c
+++ src/builtin_functions/cd.c
@@ -3,11 +3,18 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nildruon <nildruon@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nilsdruon <nilsdruon@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/26 20:59:35 by nildruon          #+#    #+#             */
-/*   Updated: 2026/05/27 14:42:10 by nildruon         ###   ########.fr       */
+/*   Updated: 2026/06/03 14:32:35 by nilsdruon        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+
+//TODO: change OLDPWD if it exits (bash doenst create one if you dont have it)
+//TODO: change PWD when you cd
+void cd(t_single_linked_node	*envp, char **input)
+{
+    
+}
