6241e9eeb2d584aedc1bb1e4b578a2cb313effc7
Nils Druon <nildruon@c1r2p7.42vienna.com>
2026-07-13 21:01:37 +0200

    changed some logic of the pipes, not yet production ready

--- src/fork_management/fork_creation.c
+++ src/fork_management/fork_creation.c
@@ -3,14 +3,14 @@
 /*                                                        :::      ::::::::   */
 /*   fork_creation.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nilsdruon <nilsdruon@student.42.fr>        +#+  +:+       +#+        */
+/*   By: nildruon <nildruon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/07 14:17:36 by nilsdruon         #+#    #+#             */
-/*   Updated: 2026/07/09 19:03:22 by nilsdruon        ###   ########.fr       */
+/*   Updated: 2026/07/13 21:00:38 by nildruon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include "../minishell.h"
+#include "../minishell.h"
 
 #include <stdio.h>
 #include <unistd.h>
@@ -18,56 +18,68 @@
 #include <sys/wait.h>
 
 
-// number of children -1
+// prev_pipe and nxt_pipe have the fds that are needed in that child and 
+// therefore shouldnt be closed at the beginning of the child
 
-static void first_child(int	*fds)
+static void first_child(int	*first_pipe, t_command	*cmd)
 {
-	
+	close(first_pipe[0]);
+	exec_command(NULL, NULL);
 }
-static void middle_children()
+static void middle_children(int	prev_read_fd, int	*next_pipe_fds, t_command	*cmd)
 {
-	
-}
-
-static void last_child()
-{
-		
+	exec_command(NULL, NULL);
 }
 
-static int parent()
+static void last_child(int	prev_read_fd, t_command	*cmd)
+{
+	close(prev_read_fd);
+	exec_command(NULL, NULL);
+}
+
+static int parent(t_single_linked_node	*cmd_lst)
 {
 	int status = 0;
-	int cnt = 10;
-	int id[cnt];
-	int fds[cnt][2];
-	
-	for(int i = 0; i < cnt; i++)
-	{
-		if(pipe(fds[i]) == -1)
-			return(1); //need to close all fds on failure
-	}
+	int i = 0;
+	int cmd_lst_size = ft_single_lstsize(cmd_lst);
+	int id[cmd_lst_size];
+	int next_pipe_fds[2];
+	int prev_read_fd;
+	t_command	*curr_cmd;
+
+	while(cmd_lst)
     {
+		if(i > 0)
+			prev_read_fd = next_pipe_fds[0];
+		if(cmd_lst->next && pipe(next_pipe_fds) == -1)
+		{
+			perror("pipe: ");
+			exit(1);
+		}
 		id[i] = fork();
 		if(id[i] == -1)
-		for(int i = 0; i <B cnt; i++)
 			perror("fork failed");
         if(id[i] == 0)
 		{
+			curr_cmd = (t_command	*)cmd_lst;
 			if(i == 0)
-				first_child(fds[i]);
-			else if(i == cnt - 1)
-				last_child(fds[i]);
+			{
+				first_child(next_pipe_fds, curr_cmd);
+			}
+			else if(!cmd_lst->next)
+				last_child(prev_read_fd, curr_cmd);
 			else
-				middle_children(fds[i]);
-			exit(i);
+				middle_children(prev_read_fd, next_pipe_fds, curr_cmd);
 		}
+		else
+		{
+			if(i > 0)
+				close(prev_read_fd);
+		}
+		i++;
+		cmd_lst = cmd_lst->next;
  	}
-	for(int i = 0; i < cnt; i++)
-	{
-		for(int j = 0; j < 2; j++)
-			close(fds[i][j]);
-	}
-	for(int i = 0; i < cnt; i++)
+	for(int i = 0; i < cmd_lst_size; i++)
 		waitpid(id[i], &status, 0);
 	if (WIFEXITED(status))
 		return (WEXITSTATUS(status));
