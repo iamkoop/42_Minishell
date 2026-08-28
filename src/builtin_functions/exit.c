a6d9f3a33208c2fdb3c3e86184b73b66286194b4
Nils Druon <nildruon@c1r1p8.42vienna.com>
2026-08-28 13:40:36 +0200

    builtin exit function is now working at full capacity

--- src/builtin_functions/exit.c
+++ src/builtin_functions/exit.c
@@ -3,16 +3,27 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nilsdruon <nilsdruon@student.42.fr>        +#+  +:+       +#+        */
+/*   By: nildruon <nildruon@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/27 14:41:45 by username          #+#    #+#             */
-/*   Updated: 2026/08/25 01:35:58 by nilsdruon        ###   ########.fr       */
+/*   Updated: 2026/08/28 13:39:28 by nildruon         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../minishell.h"
 
-static int	num_is_valid_help(char *str, int is_negative)
+static int zero(char c, int	*is_zero)
+{
+	if(c)
+	{
+		*is_zero = 0;
+		return(0);
+	}
+	*is_zero = 1;
+	return(1);
+}
+
+static int	num_is_valid_help(char *str, int is_negative, int *is_zero)
 {
 	char	*ll_max;
 	char	*ll_min;
@@ -20,44 +31,56 @@ static int	num_is_valid_help(char *str, int is_negative)
 	int		len;
 
 	ll_max = "9223372036854775807";
-	ll_min = "-9223372036854775808";
+	ll_min = "9223372036854775808";
 	i = -1;
-	if (is_negative)
-		i++;
+	while (*str && *str == '0')
+		str++;
+	if(zero(*str, is_zero))
+		return(1);
 	len = ft_strlen(str);
-	if ((!is_negative && len < 19) || (is_negative && len < 20))
+	if(len < 19)
 		return (1);
 	while (str[++i])
 	{
-		if (is_negative && (str[i] > ll_min[i]))
-			return (0);
-		if (!is_negative && (str[i] > ll_max[i]))
+		if(!ft_isdigit(str[i]))
+			break ;
+		if ((is_negative && (str[i] > ll_min[i])) ||
+			(!is_negative && (str[i] > ll_max[i])))
 			return (0);
 	}
 	return (1);
 }
 
-static int	num_is_valid(char *str)
+static int	num_is_valid(char *str, int *is_zero)
 {
 	int	i;
 	int	is_negative;
 
 	i = 0;
-	if (str[i] == '-')
+	while (*str && *str == ' ')
+		str++;
+	if (str[i] == '-' || str[i] == '+' || ft_isdigit(str[i]))
+		i++;
+	else
+		return(0);
+	while (str[i] && ft_isdigit(str[i]))
 		i++;
 	while (str[i])
 	{
-		if (!ft_isalnum(str[i++]))
+		if(str[i] != ' ')
 			return (0);
+		i++;
 	}
 	if (str[0] == '-')
 		is_negative = 1;
 	else
 		is_negative = 0;
-	return (num_is_valid_help(str, is_negative));
+	if(str[0] == '+' || str[0] == '-')
+		str++;
+	return (num_is_valid_help(str, is_negative, is_zero));
 }
 
-static void	error_msg(char *arg)
+static void	exit_error_msg(char *arg)
 {
 	ft_putstr_fd("minishell: exit: ", 2);
 	if (arg)
@@ -72,6 +95,7 @@ static void	error_msg(char *arg)
 void	builtin_exit(char **input)
 {
 	int	len;
+	int is_zero;
 
 	len = 0;
 	ft_putendl_fd("exit", 1);
@@ -82,11 +106,12 @@ void	builtin_exit(char **input)
 	{
 		if (len > 1)
 		{
-			error_msg(NULL);
+			exit_error_msg(NULL);
+			return ;
 		}
-		if (!num_is_valid(input[len]))
+		if (!num_is_valid(input[len], &is_zero))
 		{
-			error_msg(input[len]);
+			exit_error_msg(input[len]);
 			exit(2);
 		}
 		len++;
