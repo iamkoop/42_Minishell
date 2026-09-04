984b1d469e243fda5dfdacc42150f63a4fabb8e5
Barbara Stalze <barbara.stalze@protonmail.com>
2026-09-04 10:46:09 +0200

    free_env_lst.c norminetted

--- src/err_and_cleanup_funcs/free_env_lst.c
+++ src/err_and_cleanup_funcs/free_env_lst.c
@@ -12,13 +12,13 @@
 
 #include "../../minishell.h"
 
-void    free_env_lst(t_single_linked_node *env)
+void	free_env_lst(t_single_linked_node *env)
 {
 	t_single_linked_node	*curr_node;
 	t_single_linked_node	*next_node;
 
 	curr_node = env;
-	while(curr_node)
+	while (curr_node)
 	{
 		next_node = curr_node->next;
 		del_env_node_content(curr_node->content);
