6fc6401b6382031957e31671129b54cc7bc80092
Nils Druon <ndruon@outlook.com>
2026-09-05 13:46:17 +0200

    files in err_and_cleanup_funcs are normitte compliant

--- src/err_and_cleanup_funcs/exec_err_msg.c
+++ src/err_and_cleanup_funcs/exec_err_msg.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec_err_msg.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nildruon <nildruon@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nilsdruon <nilsdruon@student.42.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/08/25 12:37:36 by nildruon          #+#    #+#             */
-/*   Updated: 2026/08/27 16:08:27 by nildruon         ###   ########.fr       */
+/*   Updated: 2026/09/05 13:42:00 by nilsdruon        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,54 +16,37 @@ static char	*add_to_string(char	*curr, char	*to_append1, char	*to_append2)
 {
 	char	*ret;
 
-	if(curr)
+	if (curr)
 		ret = ft_strjoin_three(curr, to_append1, to_append2);
 	else
 		ret = ft_strdup(to_append1);
 	free(curr);
-	return(ret);
+	return (ret);
 }
 
 void	err_msg(char	*func, char *value, char	*custom_err)
-{	
+{
 	char	*whole_msg;
-	
+
 	whole_msg = add_to_string(NULL, "minishell: ", NULL);
-	if(!whole_msg)
-		return;
-	if(func)
+	if (!whole_msg)
+		return ;
+	if (func)
 	{
 		whole_msg = add_to_string(whole_msg, func, ": ");
-		if(!whole_msg)
-			return;
+		if (!whole_msg)
+			return ;
 	}
-	if(value)
+	if (value)
 	{
 		whole_msg = add_to_string(whole_msg, value, ": ");
 	}
-	if(!custom_err)
+	if (!custom_err)
 		whole_msg = add_to_string(whole_msg, strerror(errno), "");
 	else
 		whole_msg = add_to_string(whole_msg, custom_err, "");
-	if(!whole_msg)
+	if (!whole_msg)
 		return ;
 	ft_putendl_fd(whole_msg, 2);
 	free(whole_msg);
 }
-
-
-/*ft_putstr_fd("minishell: ", 2);
-	if(func)
-	{
-		ft_putstr_fd(func, 2);
-		ft_putstr_fd(": ", 2);
-	}
-	if(value)
-	{
-		ft_putstr_fd(value, 2);
-		ft_putstr_fd(": ", 2);	
-	}
-	if(!custom_err)
-		ft_putendl_fd(strerror(errno), 2);
-	else
-		ft_putendl_fd(custom_err, 2);*/
